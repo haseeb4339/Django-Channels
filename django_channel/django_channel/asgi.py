27470640063e0d268b793b5f35d6e88ecdcaa765
haseeb4339 <haseebullahbj@gmail.com>
2024-12-15 19:54:23 +0500

    configure asig file

--- django_channel/django_channel/asgi.py
+++ django_channel/django_channel/asgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channel.settings')
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
+    'websocket': URLRouter(app.routing.websocket_urlpatterns),  # replace with your websocket URL patterns  # noqa: E501
 })
